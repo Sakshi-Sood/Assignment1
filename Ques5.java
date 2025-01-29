ff1467570e6339832511165d1f673e10ab9fe209
Sakshi <sakshisood004@gmail.com>
2025-01-29 20:19:26 +0530

    Add Ques5.java to toggle specific bits in permission settings using bitwise operations

---
+++ Ques5.java
@@ -0,0 +1,21 @@
+// package Assignment1;
+import java.util.Scanner;
+public class Ques5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter current permissions (0-15):");
+        int permissions = sc.nextInt();
+
+        System.out.print("Enter the bit position to toggle(0-3): ");
+        int bitPosition = sc.nextInt();
+
+        System.out.println("Original permissions: " + Integer.toBinaryString(permissions));
+
+        int mask = 1 << bitPosition;     
+        permissions = permissions ^ mask;   
+        System.out.println("Updated permissions: " + Integer.toBinaryString(permissions));
+
+        sc.close();
+    }
+}
